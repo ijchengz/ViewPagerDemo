737838f7164ca906e2e47aca3c8af1d0fdb4aeb7
Ian Cheng <ijcheng6@gmail.com>
2022-05-17 12:35:37 +0800

    implement: links viewPager2  with fragments            links tabLayout with viewPager2               in MainActivity.kt

--- app/src/main/java/com/example/viewpagerdemo/MainActivity.kt
+++ app/src/main/java/com/example/viewpagerdemo/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.viewpagerdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 綁定 viewPager2 與 fragments
+        viewPager2.adapter = object : FragmentStateAdapter(this) {
+            override fun getItemCount() = 3
+
+            override fun createFragment(position: Int) =
+                when (position) {
+                    0 -> ScaleFragment()
+                    1 -> RotateFragment()
+                    else -> TranslateFragment()
+                }
+        }
+        // 連結 tabLayout 與 viewPager2，並設定 tab 顯示文字
+        TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
+            when (position) {
+                0 -> tab.text = "Scale"
+                1 -> tab.text = "Rotate"
+                else -> tab.text = "Translate"
+            }
+        }.attach()
     }
 }
